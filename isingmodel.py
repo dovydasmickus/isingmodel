d802f91e41a51302804ce854339606cf22149568
dovydasmickus <dovismickus@gmail.com>
2017-01-16 03:11:07 +0000

    finished monte carlo function and collected values
    Finished the monte carlo function which sweeps through the lattice and
    adjust it for equilibrium which allows for values to be collected. This
    version also includes some minor fixes.

--- isingmodel.py
+++ isingmodel.py
@@ -1,11 +1,12 @@
 import numpy as np
 import random
+import matplotlib.pyplot as plt
 
 def init_lattice(rows,columns):
     lattice = np.zeros((rows,columns))
     for i in range(rows):
         for j in range(columns):
-                lattice[i][j] = random.choice((-1,1))
+                lattice[i][j] = 1  #value doesn't matter as the monte carlo sampling will achieve equilibrium regardless. Not calling np.choice saves computation time.
     return lattice
     
 def hamiltonian_energy(lattice,lattice_size , x, y): 
@@ -47,19 +48,23 @@ def spin_flip(lattice, lattice_size, i, j, T):
     
 def monte_carlo():
     
-    lattice_size = 10
+    lattice_size = 30
+    samples = 1
     T = 0.01
     Tlist = []
     Tmax = 5
     Tstep = 0.01
     lattice = init_lattice(lattice_size, lattice_size)
+    print lattice
     lattice_energies = []
+    lattice_energy_var = []
     lattice_mag = []
+    lattice_mag_var = []
     
     while T < Tmax:
         i = 0
         
-        while i < (lattice_size**2) * 3:
+        while i < (lattice_size**2) * 5:
             x = random.randint(0,lattice_size-1)
             y = random.randint(0,lattice_size-1)
             latt_E = spin_flip(lattice, lattice_size, x, y, T)
@@ -69,23 +74,74 @@ def monte_carlo():
         i = 0
         energy = 0
         mag = 0
+        energy_arr = []
+        mag_arr = []
         
-        while i < (lattice_size** 2) * 1:
+        while i < (lattice_size** 2) * samples:
             x = random.randint(0,lattice_size-1)
             y = random.randint(0,lattice_size-1)
             temp_energy = hamiltonian_energy(lattice,lattice_size, x, y)
+            energy_arr.append(temp_energy)
+            mag_arr.append(lattice[x][y])
             energy += temp_energy
             mag += lattice[x][y]
             i += 1
             
-        energy = energy / ((lattice_size ** 2) * 1)
-        mag = mag / (((lattice_size ** 2) * 1) ** 2)
+        energy = energy / ((lattice_size ** 2) * samples)
+        mag = mag / (((lattice_size ** 2) * samples) ** 2)
         lattice_energies.append(energy)
+        lattice_energy_var.append(variance(energy_arr))
         lattice_mag.append(mag)
+        lattice_mag_var.append(variance(mag_arr))
         Tlist.append(T)
         T += Tstep
+        #print "energy values = ", energy
+    
+    specificHeat = []    
+    for i in range(len(lattice_energy_var)):
+        specificHeat.append(lattice_energy_var[i] / (Tlist[i] ** 2))
+
+    magneticSuscep = []
+    
+    for i in range(len(lattice_mag_var)):
+        magneticSuscep.append(lattice_mag_var[i] / Tlist[i])
+    
+    plt.figure(1)
+    plt.xlabel("Temperature(Kelvin)")
+    plt.ylabel("Average Lattice Energies")
+    plt.title("Average Lattice Energy vs Temperature(30x30 lattice)")
+    plt.plot(Tlist, lattice_energies, 'b--')
+    
+    plt.figure(2)
+    plt.xlabel("Temperature(Kelvin)")
+    plt.ylabel("Average Magnestism")
+    plt.title("Average Lattice Magnetism vs Temperature(30x30 lattice)")
+    plt.plot(Tlist, lattice_mag, 'b--')
         
-
-
+    plt.figure(3)
+    plt.plot(Tlist,specificHeat, 'b--')
+    plt.xlabel("Temperature(Kelvin)")
+    plt.title("Specific Heat vs Temperature(30x30 lattice)")
+    plt.ylabel("Specific Heat Capacity")
+    
+    plt.figure(4)
+    plt.plot(Tlist, magneticSuscep, 'b--')
+    plt.xlabel("Temperature(Kelvin)")
+    plt.title("Magnetic Susceptability vs Temperature(30x30 lattice)")
+    plt.ylabel("Magnetic Susceptability")
+    plt.show()
+        
+        
+def variance(valuelist):
+    value = 0
+    value_square = 0
+    i = 0
+    for n in valuelist:
+        i += 1
+        value += n
+        value_square += n*n
+    return (value_square - ((value**2)/i )) / i
+    
+    
     
 monte_carlo()
